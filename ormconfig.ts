7b8b0b492b700400b3ca0167c09850ae8eb05a15
Na Vu <vuthilyna304@gmail.com>
2022-06-01 23:47:15 +0700

    make it better

--- ormconfig.ts
+++ ormconfig.ts
@@ -5,19 +5,18 @@ import type { TypeOrmModuleOptions } from '@nestjs/typeorm';
 // import { UserSubscriber } from './src/entity-subscribers/user-subscriber';
 // import { SnakeNamingStrategy } from './src/snake-naming.strategy';
 
-const configs: TypeOrmModuleOptions & { seeds: string[]; factories: string[] } =
-  {
-    type: 'mysql',
-    host: 'bpuquygpfgcgeo4fjkks-mysql.services.clever-cloud.com',
-    port: 3306,
-    username: 'ufaqi3gzlmpfuwdr',
-    password: 'tHU17ojPwILywzgTuHf5',
-    database: 'bpuquygpfgcgeo4fjkks',
-    // namingStrategy: new SnakeNamingStrategy(),
-    // subscribers: [TGLHKNSubscriber],
-    entities: ['src/modules/**/*.entity{.ts,.js}'],
+const configs: TypeOrmModuleOptions = {
+  type: 'mysql',
+  host: 'bpuquygpfgcgeo4fjkks-mysql.services.clever-cloud.com',
+  port: 3306,
+  username: 'ufaqi3gzlmpfuwdr',
+  password: 'tHU17ojPwILywzgTuHf5',
+  database: 'bpuquygpfgcgeo4fjkks',
+  // namingStrategy: new SnakeNamingStrategy(),
+  // subscribers: [TGLHKNSubscriber],
+  entities: ['src/modules/**/*.entity{.ts,.js}'],
 
-    migrationsRun: false,
-  };
+  migrationsRun: false,
+};
 
 module.exports = configs;
